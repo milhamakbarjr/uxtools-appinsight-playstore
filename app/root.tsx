d55b36b536430a4888433f579907e5d70de52c6d
Ryujin <w230037@e.ntu.edu.sg>
2025-03-24 20:48:01 +0700

    Feat: update analytics integration in root and index components
    - Added the Analytics component from @vercel/analytics/remix to the Layout in root.tsx for improved tracking.
    - Removed the previously imported Analytics component from _index.tsx to avoid duplication.

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import { Analytics } from "@vercel/analytics/remix";
 
 import "./tailwind.css";
 
@@ -33,6 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Analytics />
         <ScrollRestoration />
         <Scripts />
       </body>
